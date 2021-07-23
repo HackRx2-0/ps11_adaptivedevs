e224456395101f4b57d2af2da982f0d24a5fd231
tasnimzotder <tasnimzotder@gmail.com>
2021-07-23 09:39:36 +0530

    :tada: add basic structure of client-web-mobile

---
+++ client-web-mobile/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{ts, tsx, js, jsx}',
+    './components/**/*.{ts, tsx, js, jsx}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
